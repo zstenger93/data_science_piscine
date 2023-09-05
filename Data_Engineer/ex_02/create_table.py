68464164e6507050238197ee3953195b3c29a484
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-09-05 16:31:12 +0200

    engineer 03

--- Data_Engineer/ex_02/create_table.py
+++ Data_Engineer/ex_02/create_table.py
@@ -1,22 +1,32 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table
 import sqlalchemy
+
+def table_exists(engine, table_name):
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    if table_name in metadata.tables:
+        print(f"Table {table_name} already exist")
+    return table_name in metadata.tables
 
 
 def load(path, tableName):
     try:
-        data = pd.read_csv(path)
-        data_types = {
-            "event_time": sqlalchemy.DateTime(),
-            "event_type":  sqlalchemy.types.String(length=255),
-            "product_id": sqlalchemy.types.Integer(),
-            "price": sqlalchemy.types.Float(),
-            "user_id": sqlalchemy.types.BigInteger(),
-            "user_session": sqlalchemy.types.Uuid()
-        }
-
         engine = create_engine("postgresql://zstenger:msp@localhost:5432/piscineds")
-        data.to_sql(tableName, engine, index=False, dtype=data_types)
+        if not table_exists(engine, tableName):
+            print(f"Table {tableName} doesn't exist, creating...")
+            data = pd.read_csv(path)
+            data = pd.read_csv(path)
+            data_types = {
+                "event_time": sqlalchemy.DateTime(),
+                "event_type": sqlalchemy.types.String(length=255),
+                "product_id": sqlalchemy.types.Integer(),
+                "price": sqlalchemy.types.Float(),
+                "user_id": sqlalchemy.types.BigInteger(),
+                "user_session": sqlalchemy.types.UUID(as_uuid=True)  # Corrected the data type
+            }
+            data.to_sql(tableName, engine, index=False, dtype=data_types)
+            print(f"Table {tableName} created")
         engine.dispose()
     except Exception as error:
         print(f"An error occurred: {error}")
--- README.md
+++ README.md
@@ -9,3 +9,13 @@ alias connect='docker exec -it postgres psql -U zstenger -d piscineds -h localho
 ```
 alias run='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker-compose up -d'
 ```
+
+# postgres command examples
+
+```
+SELECT * FROM data_2022_oct LIMIT 10;
+```
+
+```
+DROP TABLE "data_2022_oct";
+```
