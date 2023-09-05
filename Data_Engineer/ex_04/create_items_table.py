5942b3e785d71515b35e75c117f0d8675961214e
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-09-05 17:04:28 +0200

    engineer 04

--- Data_Engineer/ex_04/create_items_table.py
+++ Data_Engineer/ex_04/create_items_table.py
@@ -1,4 +1,3 @@
-import os
 import sqlalchemy
 import pandas as pd
 from tqdm import tqdm
@@ -13,27 +12,23 @@ def table_exists(engine, table_name):
     return table_name in metadata.tables
 
 
-def load(path, tableName, overall_progress_bar):
+def load(path, tableName):
     try:
         engine = create_engine("postgresql://zstenger:msp@localhost:5432/piscineds")
         if not table_exists(engine, tableName):
             print(f"\033[1;33mTable {tableName} doesn't exist, creating...\033[0;39m")
             data = pd.read_csv(path)
             data_types = {
-                "event_time": sqlalchemy.DateTime(),
-                "event_type": sqlalchemy.types.String(length=255),
-                "product_id": sqlalchemy.types.Integer(),
-                "price": sqlalchemy.types.Float(),
-                "user_id": sqlalchemy.types.BigInteger(),
-                "user_session": sqlalchemy.types.UUID(as_uuid=True)
+                "pruduct_id": sqlalchemy.types.Integer(),
+                "category_id": sqlalchemy.types.BigInteger(),
+                "category_code": sqlalchemy.types.String(length=255),
+                "brand": sqlalchemy.types.String(length=255)
             }
-
             total_rows = len(data)
             chunk_size = total_rows // 100
             chunk_count = total_rows // chunk_size
             remainder = total_rows % chunk_size
-
-            with tqdm(total=total_rows, position=1, leave=True, bar_format="{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]") as pbar:
+            with tqdm(total=total_rows, position=0, leave=True) as pbar:
                 for i in range(chunk_count):
                     chunk = data.iloc[i * chunk_size:(i + 1) * chunk_size]
                     chunk.to_sql(tableName, engine, index=False, dtype=data_types, if_exists="append")
@@ -42,28 +37,12 @@ def load(path, tableName, overall_progress_bar):
                     remainder_chunk = data.iloc[chunk_count * chunk_size:]
                     remainder_chunk.to_sql(tableName, engine, index=False, dtype=data_types, if_exists="append")
                     pbar.update(remainder)
-
             tqdm.write(f"\033[1;32mTable {tableName} created\033[0;39m")
         engine.dispose()
-        overall_progress_bar.update(1)
     except Exception as error:
         print(f"An error occurred: {error}")
 
 
-def create_tables(folder_path):
-    table_creation_count = 0
-    total_tables = len([filename for filename in os.listdir(folder_path) if filename.endswith(".csv")])
-
-    with tqdm(total=total_tables, position=0, leave=True, bar_format="\033[1;31mOverall progress:\033[0;39m {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]") as overall_pbar:
-        for filename in os.listdir(folder_path):
-            if filename.endswith(".csv"):
-                table_name = os.path.splitext(filename)[0]
-                file_path = os.path.join(folder_path, filename)
-                print("Getting next table for injection ...")
-                load(file_path, table_name, overall_pbar)
-                table_creation_count += 1
-
-
 if __name__ == "__main__":
-    folder_path = "/goinfre/zstenger/subject/customer/"
-    create_tables(folder_path)
+    print("Getting next table for injection ...")
+    load("/goinfre/zstenger/subject/item/item.csv", "items")
