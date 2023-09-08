78e615ea9e17a3eb6a7cdfe0af243ae55a9c9c1a
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-09-08 07:02:12 +0200

    really?

--- Data_Analyst/ex_01/chart.py
+++ Data_Analyst/ex_01/chart.py
@@ -1,6 +1,8 @@
 import psycopg2
 import matplotlib.pyplot as plt
 from datetime import datetime
+from matplotlib.ticker import FuncFormatter
+from matplotlib.dates import MonthLocator, DateFormatter
 
 dbname = "piscineds"
 user = "zstenger"
@@ -32,34 +34,25 @@ try:
     
     for event_time, event_type in data:
         if event_type == 'purchase':
-            # Extract the year, month, and day from the event_time
             year, month, day = event_time.year, event_time.month, event_time.day
-            
-            # Create a date object with the year, month, and day
             date = datetime(year, month, day)
-            
-            # Format the date to 'YYYY-MM-DD' string
             date_str = date.strftime('%Y-%m-%d')
-            
-            # If the date is within the range (October to February), increment the count
-            if month >= 10 or month <= 2:
+            if month >= 10 or month <= 1:
                 if date_str not in purchase_counts:
                     purchase_counts[date_str] = 0
                 purchase_counts[date_str] += 1
     
-    # Extract the dates and counts
     sorted_counts = sorted(purchase_counts.items())
     dates, counts = zip(*sorted_counts)
     
     plt.figure(figsize=(12, 8))
     plt.plot(dates, counts, linestyle='-')
     plt.ylabel("Number of customers")
-    plt.xticks(rotation=45)
-    plt.gca().get_xaxis().set_major_locator(plt.MaxNLocator(5))
-    plt.gca().get_yaxis().set_major_locator(plt.MaxNLocator(5))
+    plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda x, pos: f'{int(x / 10)}'))
+    tick_positions = [0, len(dates) // 4, 2 * len(dates) // 4, 3 * len(dates) // 4, len(dates) - 1]
+    tick_labels = ["Oct", "Nov", "Dec", "Jan", "Feb"]
+    plt.xticks(tick_positions, tick_labels)
     plt.xlim(dates[0], dates[-1])
-    plt.tight_layout()
     plt.show()
-    
 except Exception as e:
     print(f"Error: {e}")
