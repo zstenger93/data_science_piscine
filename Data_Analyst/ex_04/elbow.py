b8ec863b87c1d308d3524e774ffdcd1d9cc8f247
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-09-11 15:34:33 +0200

    analyst 04 almost

---
+++ Data_Analyst/ex_04/elbow.py
@@ -0,0 +1,43 @@
+import psycopg2
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+dbname = "piscineds"
+user = "zstenger"
+password = "msp"
+host = "localhost"
+port = "5432"
+
+try:
+    with open("elbow.sql", "r") as sql_file:
+        sql_script1 = sql_file.read()
+    print("SQL code has been imported!")
+    conn = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+    print("Connected to PostgreSQL!")
+    cursor = conn.cursor()
+    cursor.execute(sql_script1)
+    print("SQL script executed successfully!")
+    data_frequency = cursor.fetchall()
+    print("Data has been fetched from the table.")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    wss = []
+    for k in range(1, 11):
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init=10).fit(data_frequency)
+        wss.append(kmeans.inertia_)
+
+    plt.plot(range(1, 11), wss)
+    plt.xlabel("Number of clusters")
+    plt.title("The Elbow Method")
+    plt.show()
+except Exception as e:
+    print(f"Error: {e}")
